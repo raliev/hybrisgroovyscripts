fdb7b6941a752040f6e11feb68f285af75334d6a
Rauf Aliev <Rauf_Aliev@epam.com>
2017-12-18 00:04:37 -0500

    version 0.1

--- all-types-having-the-specified-type-as-an-attribute.groovy
+++ all-types-having-the-specified-type-as-an-attribute.groovy
@@ -1,27 +1,31 @@
 /* (c) 2017 Rauf Aliev, http://hybrismart.com */
 
-String pkObj     = "8796141715457"
-type 		 = "Product";
+//String pkObj     = "8797136093214"
+//String pkObj = "8796695330846";
+//String pkObj = "8797134192670";
+//String pkObj = "8796147122318"
+String pkObj = "8797134192670"
+//type 		 = "Category"  ;
 
 /****************************************************** */
+DEBUG = 1;
 import de.hybris.platform.hac.data.dto.SqlSearchResultData;
 import de.hybris.platform.hac.facade.impl.DefaultFlexibleSearchFacade;
 import de.hybris.platform.core.PK;
 import de.hybris.platform.core.model.type.TypeModel;
-import de.hybris.platform.persistence.type.ComposedTypeEJBImpl;
-import de.hybris.platform.persistence.type.ComposedTypeRemote;
-import de.hybris.platform.jalo.type.ComposedType;
 
+if (!hasProperty('type')) { type = modelService.get(new PK(pkObj as Long)).itemtype as String; }
 
-obj = modelService.get(new PK(pkObj as Long));
-types = []
+types = getSubTypes(type, new ArrayList()) + getSuperTypes(type, new ArrayList());
 types.add(type);
 List<String> queries = new ArrayList<>();
 for (String childType in types) {
+    if (DEBUG) { println ("type="+ childType )}
     queries.addAll(whereThisTypeIsUsed(childType, pkObj));
 }
 String finalQuery= queries.join("\n UNION \n").replace("<condition>","").replace("</condition>","");
 flexibleSearchFacade = new DefaultFlexibleSearchFacade();
+if (DEBUG) { println "Final Query: \n"+finalQuery; }
 result = flexibleSearchFacade.executeRawSql(finalQuery, 2000000, false);
 
 for (item in result.getResultList()) {
@@ -60,45 +64,132 @@ List<String> getSubTypes (String type, List<String> eSubTypes)
 List<String> whereThisTypeIsUsed (String requestedType, String pkObj) {
 
     typeService = spring.getBean("typeService");
-    typePK = typeService.getTypeForCode(requestedType).getPk();
+    requestedItemType = typeService.getTypeForCode(requestedType);
+    typePK = requestedItemType.getPk();
 
     SqlSearchResultData searchResult;
-    query =
-            "select OwnerPkString, columnName from attributedescriptors where AttributeTypePK = '"+typePK + "' and columnName <> ''";
-    ;
+    query = /select OwnerPkString, columnName, 0, 0 from attributedescriptors where (AttributeTypePK ='/+typePK + /'  and columnName <> '')
+    union
+    select attributedescriptors.OwnerPkString,  attributedescriptors.columnName, 1, 0
+    from attributedescriptors, collectiontypes as a 
+    where
+      (a.PK = attributedescriptors.AttributeTypePK  
+       and a.elementTypePk = '/+typePK+/'
+       and columnName <> ""
+       )
+       union
+   select distinct a.p_targettype, attributedescriptors.columnName, 2, ydeployments.TableName
+ from attributedescriptors, composedtypes as a
+ left join ydeployments on a.itemtypecode  = ydeployments.typecode 
+    where
+      (attributedescriptors.p_relationname=a.InternalCode 
+       and a.p_sourcetype =  '/+typePK+/'          
+       and columnName is null
+       and tableName is not null
+       )
+union       
+select distinct  attributedescriptors.OwnerPkString, attributedescriptors.columnName, 3, ydeployments.TableName
+ from attributedescriptors, composedtypes as a
+ left join ydeployments on a.itemtypecode  = ydeployments.typecode 
+    where
+      (attributedescriptors.p_relationname=a.InternalCode 
+       and a.p_sourcetype = '/+typePK+/'       
+       and columnName is not null       
+       )      
+
+     union
+   select distinct a.p_sourcetype, attributedescriptors.columnName, "2_", ydeployments.TableName
+ from attributedescriptors, composedtypes as a
+ left join ydeployments on a.itemtypecode  = ydeployments.typecode 
+    where
+      (attributedescriptors.p_relationname=a.InternalCode 
+       and a.p_targettype =  '/+typePK+/'          
+       and columnName is null
+       and tableName is not null
+       )
+union       
+select distinct  attributedescriptors.OwnerPkString, attributedescriptors.columnName, "3_", ydeployments.TableName
+ from attributedescriptors, composedtypes as a
+ left join ydeployments on a.itemtypecode  = ydeployments.typecode 
+    where
+      (attributedescriptors.p_relationname=a.InternalCode 
+       and a.p_targettype = '/+typePK+/'       
+       and columnName is not null       
+       )  
+        
+       
+       /
     flexibleSearchFacade = new DefaultFlexibleSearchFacade();
     result = flexibleSearchFacade.executeRawSql(query, 2000000, false);
+    if (DEBUG) { println "query=" + query; }
     resultList = [];
     for (item in result.getResultList()) {
         enclosingTypePk = item[0];
         enclosingType = modelService.get(new PK(enclosingTypePk as Long));
         if (enclosingType) {
             itemtype = typeService.getTypeForCode(enclosingType.getCode());
-            if (itemtype && !itemtype.getAbstract()) {
-                table = itemtype.getTable();
-
+            boolean abstr = true;
+            collectiontype = 0;
+            String field = item[1];
+            if (item[2] == "1") {
+                abstr = false;
+                table = requestedItemType.getTable();
+                collectiontype = 1;
+            }
+            if (item[2] == "0" || item[2] == "3" || item[2] == "3_") {
+                if (itemtype.getClass().getSimpleName().contains("ComposedType")) {
+                    if (itemtype.getAbstract()) {
+                        abstr = true;
+                    } else {
+                        abstr = false; table = itemtype.getTable();
+                    }
+                }
+            }
+            if (item[2] == "2" ) {
+                table = item[3];
+                field = "SourcePK";
+                abstr = false;
+            }
+            if (item[2] == "2_" ) {
+                table = item[3];
+                field = "TargetPK";
+                abstr = false;
+            }
+            if (itemtype && !abstr) {
                 blacklistTables = ["attributedescriptors", "maptypes", "collectiontypes", "widgetparameter", "savedvalues", "savedqueries"]
                 blacklistAttrs  = ["TypePkString"]
                 if (table
                         && !blacklistTables.contains(table)
-                        && !blacklistAttrs.contains(item[1])) {
-                    String select = query = "SELECT '" +
-                            enclosingType.getCode() +
-                            "', '" +
-                            table;
-                    String condition = "\n       (" + table + "." + item[1] + " = '" + pkObj + "'" +
-                            " AND " +
-                            " TypePkString = " + item[0] + ")";
-                    String select2 =
-                            "', \"<condition>" +
-                                    condition.replace("\n","").trim() +
-                                    "</condition>\", " +
-                                    table + ".pk ";
-                    String from =
-                            "\n    FROM " + table +
-                                    "\n    WHERE ";
+                        && !blacklistAttrs.contains(item[1])) { /* not for collections!*/
 
-                    String query = select + select2 + from + condition;
+                    String select = query = "SELECT '" +enclosingType.getCode() + "'," +
+                            "'" + table + "',";
+                    String condition = "";
+                    String from = "";
+                    if (collectiontype) {
+                            condition = "\n       (" + enclosingType.getTable() + "." + item[1] + " LIKE '%" + pkObj + "%')";
+                            from =      "\n    FROM " + enclosingType.getTable() +
+                                        "\n    WHERE ";
+                            select3 = enclosingType.getTable() + ".pk ";
+                    } else {
+                            condition = "\n       (" + table + "." + field + " = '" + pkObj + "'";
+                            if (item[2] != "2" && item[2] != "2_") {
+                                condition = condition +
+                                        " AND " +
+                                    " TypePkString = " + item[0] ;
+                                }
+                                condition = condition + ")"
+                            from =
+                                "\n    FROM " + table +
+                                        "\n    WHERE ";
+                            select3 = table + ".pk ";
+                    }
+                    String select2 =
+                            "\"<condition>" +
+                            condition.replace("\n","").trim() +
+                            "</condition>\", ";\
+
+                    String query = select + select2 + select3 + from + condition;
                     fired = 0;
                     if (!resultList.contains(query)) {
                         for (Integer i=0; i<resultList.size(); i++) {
@@ -120,12 +211,3 @@ List<String> whereThisTypeIsUsed (String requestedType, String pkObj) {
     }
     return resultList;
 }
-
-
-
-/*println result.getHeaders().join("\t");
-
-for (item in result.getResultList()) {
-  println (item.join("\t"));
-}
-*/
